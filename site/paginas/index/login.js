18b9d31be4e7ef561d289eea6338ce30586b0857
helena furtado <helenafurtado25@gmail.com>
2026-01-31 19:20:55 -0300

    arrumando o projeto

--- site/paginas/index/login.js
+++ site/paginas/index/login.js
@@ -1,5 +1,5 @@
 // Espera o html carregar completamente antes de executar o codigo
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { //capturar apenas elementos (botao e campos)
 
     //botao logar
     const botaoLogar = document.getElementById("btn-logar");
@@ -8,9 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // verifica se o botao foi encontrado 
     if (botaoLogar) {
         // adiciona um ouvinte para o evento clique
-        event.preventDefault(); 
         botaoLogar.addEventListener("click", function () {
+            event.preventDefault(); // previne submissao do formulario
+            const campoEmail = document.getElementById("email").value.trim();
+            const campoSenha = document.getElementById("senha").value.trim();
 
+            if (campoEmail === "" || campoSenha === "") {
+                alert("Por favor, preencha os campos obrigatórios")
+                return;
+            }
             // redireciona para a pagina do formulario
             window.location.href = "../cadastro/formulario.html";
         }
